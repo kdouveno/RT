996b43fbd0f2fb0ea97cac7214c8d80ef15c1d98
Guillaume PEREZ <gperez@e3r11p2.42.fr>
2018-10-15 12:51:02 +0200

    debug check rot

--- srcs/parser/f_cam_parse.c
+++ srcs/parser/f_cam_parse.c
@@ -6,7 +6,7 @@
 /*   By: gperez <gperez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/04 16:49:52 by gperez            #+#    #+#             */
-/*   Updated: 2018/10/15 12:36:04 by gperez           ###   ########.fr       */
+/*   Updated: 2018/10/15 12:49:26 by gperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,17 +55,17 @@ int		check_rot(void *cam, char* l1, char *l2)
 	t_obj *obj;
 
 	obj = (t_obj*)cam;
-	if (!(ft_strcmp(l1, "dirx")))
+	if (!((ft_strcmp(l1, "dirx") && (ft_strcmp(l1, "rx")))))
 	{
 		obj->r.x = ft_atod(l2);
 		return (0);
 	}
-	if (!(ft_strcmp(l1, "diry")))
+	if (!((ft_strcmp(l1, "diry") && (ft_strcmp(l1, "ry")))))
 	{
 		obj->r.y = ft_atod(l2);
 		return (0);
 	}
-	if (!(ft_strcmp(l1, "dirz")))
+	if (!((ft_strcmp(l1, "dirz") && (ft_strcmp(l1, "rz")))))
 	{
 		obj->r.z = ft_atod(l2);
 		return (0);
