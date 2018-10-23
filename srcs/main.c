f22be6ff1b481f57e6d31426781e6c320dc52ed3
Karl DOUVENOT <kdouveno@e3r4p12.42.fr>
2018-10-23 18:38:35 +0200

    init kdouveno

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gperez <gperez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 13:28:33 by gperez            #+#    #+#             */
-/*   Updated: 2018/10/22 15:59:47 by kdouveno         ###   ########.fr       */
+/*   Updated: 2018/10/23 18:33:29 by kdouveno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,22 +41,24 @@ static void	init_cam(t_env *e)
 	}
 }
 
+start_rendering(t_env *e, int i)
+
 static void	ft_window(t_env *e)
 {
-	/*if ((e->mlx.ptr = mlx_init()) == NULL
+	if ((e->mlx.ptr = mlx_init()) == NULL
 	|| (e->mlx.win = mlx_new_window(e->mlx.ptr, DIMX,
 		DIMY, "RT")) == NULL
 	|| (e->mlx.imgptr = mlx_new_image(e->mlx.ptr, DIMX, DIMY)) == NULL
 	|| (e->mlx.img = (int*)mlx_get_data_addr(e->mlx.imgptr, e->mlx.imgarg,
 		e->mlx.imgarg + 1, e->mlx.imgarg + 2)) == NULL)
 		error(e, MLX_ERROR);
-	*/
+
 	init_cam(e);
-	/*
+	rendering()
 	mlx_put_image_to_window(e->mlx.ptr, e->mlx.win, e->mlx.imgptr, 0, 0);
 	mlx_hook(e->mlx.win, KeyPress, KeyPressMask, my_key, e);
 	mlx_loop(e->mlx.ptr);
-*/}
+}
 
 int		main(int argc, char **argv)
 {
