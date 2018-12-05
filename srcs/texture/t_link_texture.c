f37de9eca514bea6365dfeed512aa0ceff5650d8
Guillaume <gperez@student.42fr>
2018-12-05 14:12:52 +0100

    secure texture

--- srcs/texture/t_link_texture.c
+++ srcs/texture/t_link_texture.c
@@ -14,15 +14,18 @@
 
 void	link_texture(t_env *e, t_obj *obj, char *file)
 {
-	if (!file)
-		error(e, MALLOC_ERROR);
-	if (!(obj->mat.txt = SDL_LoadBMP(file)))
+	if (!(obj->mat.txt))
 	{
-		ft_putendl("\033[38;5;203m"TEXT_ERROR"\033[0m");
-		obj->mat.txt = SDL_CreateRGBSurface(0, 0, 0, 32,
-			RMASK, GMASK, BMASK, AMASK);
-		obj->mat.txt->userdata = "none";
+		if (!file)
+			error(e, MALLOC_ERROR);
+		if (!(obj->mat.txt = SDL_LoadBMP(file)))
+		{
+			ft_putendl("\033[38;5;203m"TEXT_ERROR"\033[0m");
+			obj->mat.txt = SDL_CreateRGBSurface(0, 0, 0, 32,
+				RMASK, GMASK, BMASK, AMASK);
+			obj->mat.txt->userdata = "none";
+		}
+		else
+			obj->mat.txt->userdata = "fill";
 	}
-	else
-		obj->mat.txt->userdata = "fill";
 }
