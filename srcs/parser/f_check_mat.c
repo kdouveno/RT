8ff99b976e8aa326bb6b7620c06e559eff73269d
kdouveno <kdouveno@student.42.fr>
2019-01-16 14:45:03 +0100

    correction brackets pété check mat

--- srcs/parser/f_check_mat.c
+++ srcs/parser/f_check_mat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   f_check_mat.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gperez <gperez@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kdouveno <kdouveno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 15:14:11 by gperez            #+#    #+#             */
-/*   Updated: 2019/01/13 16:16:54 by gperez           ###   ########.fr       */
+/*   Updated: 2019/01/16 14:26:40 by kdouveno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ int		check_mat2(t_env *e, t_obj *obj, char *l1, char *l2)
 {
 	if (!(ft_strcmp(l1, "c")) || !(ft_strcmp(l1, "color")))
 		parse_color(obj, l2, NULL, 0);
-	else if (!(ft_strcmp(l1, "reflexion") || !ft_strcmp(l1, "refl")))
+	else if (!ft_strcmp(l1, "reflexion") || !ft_strcmp(l1, "refl"))
 		obj->mat.refl = get_coef(ft_atod(l2));
-	else if (!(ft_strcmp(l1, "txt")) || !(ft_strcmp(l1, "texture")))
+	else if (!ft_strcmp(l1, "txt") || !ft_strcmp(l1, "texture"))
 		link_texture(e, obj, ft_strjoin("textures/", l2), 't');
-	else if (!(ft_strcmp(l1, "txt_bm")) || !(ft_strcmp(l1, "texture_bump")))
+	else if (!ft_strcmp(l1, "txt_bm") || !ft_strcmp(l1, "texture_bump"))
 		link_texture(e, obj, ft_strjoin("textures/", l2), 'b');
-	else if (!(ft_strcmp(l1, "offx")))
+	else if (!ft_strcmp(l1, "offx"))
 		obj->mat.offx = ft_atod(l2) < 0.0f ? -ft_atod(l2) : ft_atod(l2);
-	else if (!(ft_strcmp(l1, "offy")))
+	else if (!ft_strcmp(l1, "offy"))
 		obj->mat.offy = ft_atod(l2) < 0.0f ? -ft_atod(l2) : ft_atod(l2);
-	else if (!(ft_strcmp(l1, "b_lvl")) || !(ft_strcmp(l1, "bump_level")))
+	else if (!ft_strcmp(l1, "b_lvl") || !ft_strcmp(l1, "bump_level"))
 		obj->mat.b_lvl = ft_atod(l2);
 	else
 		return (1);
